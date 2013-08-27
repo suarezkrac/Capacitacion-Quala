d8b1a75b5ec3e09a483ef3884d6e28aa88b54deb
Giovanny Suarez <suarezkrac@gmail.com>
2013-08-26 19:02:08 -0500

    ejericio

---
+++ MiPrimeraApp/MiPrimeraApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiPrimeraApp
+//
+//  Created by Estudiante on 26/08/13.
+//  Copyright (c) 2013 Cymetria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiPrimeraApp/MiPrimeraApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MiPrimeraApp
+//
+//  Created by Estudiante on 26/08/13.
+//  Copyright (c) 2013 Cymetria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
