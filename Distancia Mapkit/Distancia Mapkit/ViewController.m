92a30dee24175fa4988c29c4d2c389c8006b6a75
Giovanny Suarez <suarezkrac@gmail.com>
2013-09-19 17:57:16 -0500

    archivos

--- Distancia Mapkit/Distancia Mapkit/ViewController.m
+++ Distancia Mapkit/Distancia Mapkit/ViewController.m
@@ -46,6 +46,44 @@
     pun.coordinate = coord;
     [mapVIew addAnnotation:pun];
     
+    CLLocation *loc1 = [[CLLocation alloc] initWithLatitude:22.569722 longitude:88.369722];
+    
+    CLLocation *loc2 = [[CLLocation alloc] initWithLatitude:19.12 longitude:73.02];
+    
+    //CALCULAR DISTANCIA ENTRE PUNTOS
+    // 1 Meter = 0.000621371192 Miles
+    CLLocationDistance distance = [loc1 distanceFromLocation:loc2 ];
+    NSLog(@"Distancia entre los puntos en metros es : %f \n en Millas = %f \n en Kilometros %f", distance, distance * 0.000621371192, distance/ 1000);
+    
+    
+    //LINEA
+    MKMapPoint * pointsArray = malloc(sizeof(CLLocationCoordinate2D)*2);
+    
+    CLLocationCoordinate2D coord1 = {22.569722, 88.369722};
+    CLLocationCoordinate2D coord2 = {19.12, 73.02};
+    
+    pointsArray[0] = MKMapPointForCoordinate(coord1);
+    pointsArray[1] = MKMapPointForCoordinate(coord2);
+    
+    routeLine = [MKPolyline polylineWithPoints:pointsArray count:2];
+    
+    [mapVIew addOverlay:routeLine];
+}
+
+//METODO DEL MAP DELEGATE
+-(MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay{
+    
+    MKOverlayView * overlayView = nil;
+    
+    MKPolylineView * _routeLineView = [[MKPolylineView alloc] initWithPolyline:self.routeLine];
+    _routeLineView.fillColor = [UIColor redColor];
+    _routeLineView.strokeColor = [UIColor greenColor];
+    _routeLineView.lineWidth = 5;
+    _routeLineView.lineCap = kCGLineCapSquare;
+    
+    overlayView = _routeLineView;
+    
+    return overlayView;
 }
 
 - (void)didReceiveMemoryWarning
